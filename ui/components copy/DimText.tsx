281750e8e82800295676c2e5cea78ebbeb61e938
Moti Zilberman <moti@meta.com>
2023-10-13 22:06:32 +0100

    Delete accidental commit of `ui/components copy`

--- ui/components copy/DimText.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from 'react';
-
-export function DimText({ children }: { children: ReactNode }) {
-  return <span className="text-gray-600 dark:text-gray-400">{children}</span>;
-}
