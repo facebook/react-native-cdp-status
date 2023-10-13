4fa56d7fbbc80549f135b855d1fd6250fd9ee09d
Moti Zilberman <moti@meta.com>
2023-10-13 03:38:08 +0100

    Sort domain members per upstream docs

--- data/protocols.tsx
+++ data/protocols.tsx
@@ -4,19 +4,50 @@
  * https://github.com/ChromeDevTools/debugger-protocol-viewer/blob/85935f731864d54fa60d67548d2fd9862ef4014d/make-stable-protocol.js
  * https://github.com/ChromeDevTools/debugger-protocol-viewer/blob/85935f731864d54fa60d67548d2fd9862ef4014d/merge-protocol-files.js
  * https://github.com/ChromeDevTools/debugger-protocol-viewer/blob/85935f731864d54fa60d67548d2fd9862ef4014d/prep-tot-protocol-files.sh
+ * https://github.com/ChromeDevTools/debugger-protocol-viewer/blob/85935f731864d54fa60d67548d2fd9862ef4014d/pages/domainGenerator.js
  */
 
 import DevToolsBrowserProtocol from 'devtools-protocol/json/browser_protocol.json';
 import DevToolsJsProtocol from 'devtools-protocol/json/js_protocol.json';
 
 const totProtocol = {
-  domains: [
-    ...DevToolsBrowserProtocol.domains,
-    ...DevToolsJsProtocol.domains,
-  ].sort((a, b) => a.domain.localeCompare(b.domain)),
+  domains: [...DevToolsBrowserProtocol.domains, ...DevToolsJsProtocol.domains]
+    .map(sortDomainMembers)
+    .sort((a, b) => a.domain.localeCompare(b.domain)),
 } as const;
 
-export type ProtocolDomain = (typeof totProtocol.domains)[number];
+export type ProtocolDomain = (
+  | typeof DevToolsJsProtocol
+  | typeof DevToolsBrowserProtocol
+)['domains'][number];
+
+type DomainMemberBase = {
+  experimental?: boolean;
+  deprecated?: boolean;
+  optional?: boolean;
+} & ({ name: string } | { id: string });
+
+function memberSort(a: DomainMemberBase, b: DomainMemberBase) {
+  if (a.experimental !== b.experimental) return a.experimental ? 1 : -1;
+  if (a.deprecated !== b.deprecated) return a.deprecated ? 1 : -1;
+  if (a.optional !== b.optional) return a.optional ? 1 : -1;
+  const aKey = 'name' in a ? a.name : a.id;
+  const bKey = 'name' in b ? b.name : b.id;
+  return aKey.localeCompare(bKey);
+}
+
+function sortDomainMembers(domain: ProtocolDomain): ProtocolDomain {
+  return {
+    ...domain,
+    commands: [...domain.commands].sort(memberSort),
+    ...(domain.events && {
+      events: [...domain.events].sort(memberSort),
+    }),
+    ...(domain.types && {
+      types: [...domain.types].sort(memberSort),
+    }),
+  } as ProtocolDomain;
+}
 
 const isNotExperimentalOrDeprecated = <
   T extends {
